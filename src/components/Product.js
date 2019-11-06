b93d4470ded951eab4ce1c4fdce98e7e6a639a31
Alex Mendes <mendes.develop@gmail.com>
2019-11-06 16:10:44 -0500

    Styling image container

--- src/components/Product.js
+++ src/components/Product.js
@@ -31,5 +31,59 @@ export default class Product extends React.Component {
 }
 
 const ProductWrapper = styled.div`
+  .card {
+    border-color: transparent;
+    transition: all 1s linear;
+  }
+
+  .card-footer {
+    background: transparent;
+    border-top: transparent;
+    transition: all 1s linear;
+  }
+
+  &:hover {
+    .card {
+      border: 0.04rem solid rgba(0, 0, 0, 0.2);
+      box-shadow: 2px 2px 5px 0px rgba(0, 0, 0, 0.2);
+    }
+    .card-footer {
+      background: rgba(247, 247, 247);
+    }
+  }
+
+  .img-container {
+    position: relative;
+    overflow: hidden;
+  }
+
+  .card-img-top {
+    transition: all 1s linear;
+  }
+
+  .img-container:hover .card-img-top {
+    transform: scale(1.2)
+  }
+
+
+
+
+
+
+
+
+
 
 `
+
+
+
+
+
+
+
+
+
+
+
+//
